3b8960256b411b6ddd6b7769cfed11dd44733e4d
Unknown <zemoto@gmail.com>
2019-01-31 15:24:50 -0500

    Pad the filenames with 0's so they are all equal length for easy sorting

--- FolderSerializer/Program.cs
+++ FolderSerializer/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 
@@ -11,11 +12,13 @@ namespace FolderSerializer
          var currDirectory = Directory.GetCurrentDirectory();
          var filePaths = Directory.GetFiles( currDirectory ).ToList();
          filePaths.Remove( Assembly.GetExecutingAssembly().Location );
+         var numDigits = (int)Math.Floor( Math.Log10( filePaths.Count ) + 1 );
 
          int index = 1;
          foreach( var filePath in filePaths )
          {
-            var newFileName = index + Path.GetExtension( filePath );
+            var currNumDigits = (int)Math.Floor( Math.Log10( index ) + 1 );
+            var newFileName = new string( '0', numDigits - currNumDigits ) + index + Path.GetExtension( filePath );
             var newFilePath = Path.Combine( currDirectory, newFileName );
             File.Move( filePath, newFilePath );
             index++;
