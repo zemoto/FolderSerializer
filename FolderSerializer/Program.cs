a14354b73dbf64c326c8b987e5a7a333eb1514a2
Unknown <zemoto@gmail.com>
2019-01-31 15:24:15 -0500

    Add logic for basic serialization

--- FolderSerializer/Program.cs
+++ FolderSerializer/Program.cs
@@ -1,9 +1,25 @@
-﻿namespace FolderSerializer
+﻿using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace FolderSerializer
 {
    internal sealed class Program
    {
       private static void Main( string[] args )
       {
+         var currDirectory = Directory.GetCurrentDirectory();
+         var filePaths = Directory.GetFiles( currDirectory ).ToList();
+         filePaths.Remove( Assembly.GetExecutingAssembly().Location );
+
+         int index = 1;
+         foreach( var filePath in filePaths )
+         {
+            var newFileName = index + Path.GetExtension( filePath );
+            var newFilePath = Path.Combine( currDirectory, newFileName );
+            File.Move( filePath, newFilePath );
+            index++;
+         }
       }
    }
 }
