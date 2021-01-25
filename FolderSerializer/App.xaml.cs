a8a9a6322edd06f4d53ce60bce61b2501870808c
David <none@none.com>
2021-01-25 18:44:05 -0500

    Convert to WPF app with window

---
+++ FolderSerializer/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace FolderSerializer
+{
+   public partial class App
+   {
+      private void OnStartup( object sender, StartupEventArgs e )
+      {
+         var window = new MainWindow();
+         window.ShowDialog();
+      }
+   }
+}
---
+++ FolderSerializer/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Windows;
+
+namespace FolderSerializer
+{
+   public partial class MainWindow
+   {
+      public MainWindow()
+      {
+         InitializeComponent();
+      }
+
+      private void OnButtonClick( object sender, RoutedEventArgs e )
+      {
+         //TODO 
+         // - Starting Number
+         // - Digits
+         // - Numbers to Skip
+
+         int startingNumber = 1;
+         int numDigits = -1;
+         IEnumerable<int> numbersToSkip = null;
+
+         var directory = Directory.GetCurrentDirectory();
+
+         var filePaths = Directory.GetFiles( directory ).OrderBy( x => Path.GetFileNameWithoutExtension( x ) ).ToList();
+         filePaths.Remove( Assembly.GetExecutingAssembly().Location );
+
+         if ( numDigits == -1 )
+         {
+            numDigits = (int)Math.Floor( Math.Log10( filePaths.Count() ) + 1 );
+         }
+
+         if ( Serializer.SerializeFilesInDirectory( directory, filePaths, startingNumber, numDigits, numbersToSkip ) )
+         {
+            MessageBox.Show( "Serialization Success" );
+         }
+         else
+         {
+            MessageBox.Show( "Serialization Failed" );
+         }
+      }
+   }
+}
